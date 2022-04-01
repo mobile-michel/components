437fce347ac59eb39753247d3a83c500a5f93b9b
mobile-michel <info@maillard.li>
2022-04-01 17:48:33 +0200

    Intermediate update

--- src/config.ts
+++ src/config.ts
@@ -9,13 +9,12 @@ export const NAV_ITEMS = {
         path: './',
         title: 'Home'
     },
-    list: {
-        path: './component1',
-        title: 'First component'
+    card: {
+        path: './card',
+        title: 'Cards'
+    },
+    footer: {
+        path: './footer',
+        title: 'Footers'
     }
-}
-
-export const PHOTOS = {
-    photo01: 'https://images.unsplash.com/photo-1490129375591-2658b3e2ee50?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2244&q=80',
-    photo02: 'https://images.unsplash.com/photo-1433838552652-f9a46b332c40?auto=format&fit=crop&w=800&q=80'
 }
