9662c5efeb16b5899325da4499964094fceacfcc
mobile-michel <info@maillard.li>
2022-04-01 12:20:59 +0200

    Intermediate update

---
+++ src/config.ts
@@ -0,0 +1,23 @@
+export const SITE = {
+    title: 'World Best-selling Airplanes',
+    description: 'Best-selling civil airplanes of all time, and what made them stand out from the rest? Total deliveries as of December 31, 2021.',
+    source: 'CNN Travel',
+    link: 'https://edition.cnn.com/travel/article/world-best-selling-airplanes/',
+    footer: '© 2022 Wikipedia & CNN Travel'
+}
+
+export const NAV_ITEMS = {
+    home: {
+        path: './index.html',
+        title: 'Home'
+    },
+    list: {
+        path: './aircraft/boeing737.html',
+        title: 'List'
+    }
+}
+
+export const PHOTOS = {
+    photo01: 'https://images.unsplash.com/photo-1490129375591-2658b3e2ee50?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2244&q=80',
+    photo02: 'https://images.unsplash.com/photo-1433838552652-f9a46b332c40?auto=format&fit=crop&w=800&q=80'
+}
